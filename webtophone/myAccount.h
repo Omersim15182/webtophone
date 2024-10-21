c4d8c8b39e3e1481128dfefaf74325b8147ca1b3
Omersim15182 <153455808+Omersim15182@users.noreply.github.com>
2024-10-21 13:53:16 +0300

    Update myAccount.h

--- webtophone/myAccount.h
+++ webtophone/myAccount.h
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <pjsua2.hpp>
 #include <pjsip.h>
+#include <map>
+#include <string>
 
 using namespace pj;
 
@@ -12,6 +14,14 @@ class MyAccount : public Account {
 public:
     virtual void onRegState(OnRegStateParam& prm) override;
     virtual void onIncomingCall(OnIncomingCallParam& iprm) override;
+
+
+    void addAccount(const std::string& phoneNumber, const AccountConfig& acfg);
+    MyAccount* getAccountByPhoneNumber(const std::string& phoneNumber);
+
+
+private:
+    std::map<std::string, MyAccount*> accounts_;
 };
 
 #endif // MY_ACCOUNT_H
